ffbb68a6f1e35195172328a4756dd3a0923deb0a
andrvoitovych <andrvoitovych@gmail.com>
2020-05-08 09:46:52 +0300

    add missed export types for TL table

--- Kraken.WebUI/ClientApp/src/components/transmission-loss-table/transmission-loss-table.js
+++ Kraken.WebUI/ClientApp/src/components/transmission-loss-table/transmission-loss-table.js
@@ -2,6 +2,9 @@
 import { Table, Button, ButtonGroup } from 'reactstrap';
 import { exportTableToCsv } from '../../utilites/export-table-to-csv';
 import { exportTableToExcel } from '../../utilites/export-table-to-excel';
+import { exportAsJson } from '../../utilites/export-as-json';
+import { exportAsXml } from '../../utilites/export-as-xml';
+import { jsonToXml } from '../../utilites/json-to-xml-convert';
 
 export default class TransmissionLossTable extends Component {
     tableId = "transmission-loss-table";
@@ -10,12 +13,15 @@ export default class TransmissionLossTable extends Component {
         const { data, receiverDepth, sourceDepth } = this.props;      
 
         const tableExportName = `transmission-loss-sd-${parseFloat(sourceDepth).toFixed(5)}-rd-${parseFloat(receiverDepth).toFixed(5)}`;
+        const jsonText = JSON.stringify(data);
 
         return (
             <div>
                 <div className='d-flex justify-content-end'>
                     <div>
                         <ButtonGroup>
+                            <Button outline color="warning" onClick={() => exportAsJson(jsonText, tableExportName)}>Save as JSON</Button>
+                            <Button outline color="primary" onClick={() => exportAsXml(jsonToXml(jsonText), tableExportName)}>Save as XML</Button>
                             <Button outline color="success" onClick={() => exportTableToCsv(this.tableId, tableExportName)}>Save as .csv</Button>
                             <Button outline color="success" onClick={() => exportTableToExcel(this.tableId, tableExportName)}>Save as .xls</Button>
                         </ButtonGroup>
